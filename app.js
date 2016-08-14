deeb89ae6dc55453aba736edee6bce7d5a549d45
nishoknm <nishok.n.m@gmail.com>
2016-08-13 21:58:28 -0400

    Node > 6 changes Proper error messages (404)

--- app.js
+++ app.js
@@ -13,10 +13,8 @@ app.use(express.static(__dirname + '/views'));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res) {
+    res.status(404).send('Not Found Error');
 });
 
 // error handlers
